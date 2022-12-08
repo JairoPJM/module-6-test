eba9ad176268cc276a22ca6881fc99c77b6d18a0
JairoPJM <111899130+JairoPJM@users.noreply.github.com>
2022-12-08 13:22:09 -0500

    added

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,22 @@
 
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+var timeNow=moment().format("MMMM Do YYYY")
+console.log(timeNow)
+var day1=moment().add([1], 'days').format("MMMM Do YYYY")
+var day2=moment().add([2], 'days').format("MMMM Do YYYY")
+var day3=moment().add([3], 'days').format("MMMM Do YYYY")
+var day4=moment().add([4], 'days').format("MMMM Do YYYY")
+var day5=moment().add([5], 'days').format("MMMM Do YYYY")
+
+console.log(day1)
+console.log(day2)
+console.log(day3)
+console.log(day4)
+console.log(day5)
+
+
 var input=document.querySelector("#fname")
 var wBtn=document.querySelector("#btn")
 wBtn.addEventListener("click",anything)
@@ -12,8 +28,13 @@ function anything(){
 
     fetch(urlWeather)
     .then(response=>response.json())
-    .then(data=>console.log(data.list[0].main.temp))
-}
+    .then(function(data){
+        console.log(data)
+        console.log(data.list[0].main.temp)
+        console.log(data.list[0].wind.speed)
+        console.log(data.list[0].main.humidity)
+
+    })}
 
 function name(params) {
     
@@ -27,3 +48,4 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
+
